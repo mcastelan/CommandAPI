7d56d63eaf37140cb69d20e04ae5446295f3349b
MArco <marcocastelan@gmail.com>
2021-09-06 13:57:49 -0500

    gitignore modified

--- src/CommandAPI/Startup.cs
+++ src/CommandAPI/Startup.cs
@@ -2,23 +2,35 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CommandAPI.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using CommandAPI.Data;
+using Npgsql;
+using Microsoft.EntityFrameworkCore;
 
 namespace CommandAPI
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<CommandContext>(opt=>opt.UseNpgsql
+            (Configuration.GetConnectionString("PostgreSqlConnection")));
             services.AddControllers();
-            services.AddScoped<ICommandAPIRepo,MockCommandAPIRepo>();
+            services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -31,14 +43,15 @@ namespace CommandAPI
 
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                // endpoints.MapGet("/", async context =>
-                // {
-                //     await context.Response.WriteAsync("Hello World!");
-                // });
-                endpoints.MapControllers();
-            });
+            app
+                .UseEndpoints(endpoints =>
+                {
+                    // endpoints.MapGet("/", async context =>
+                    // {
+                    //     await context.Response.WriteAsync("Hello World!");
+                    // });
+                    endpoints.MapControllers();
+                });
         }
     }
 }
